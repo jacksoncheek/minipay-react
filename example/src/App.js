0cc51b9a2284a5df738df0752fae970b65091322
Jackson Cheek <jackson.cheek@gmail.com>
2023-03-30 22:49:36 -0400

    Fix example app compilation

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,6 @@ import {
     MinipayAuthorizeApp,
     MinipayPostUsageEvent
 } from 'minipay-react'
-import 'minipay-react/dist/index.css'
 
 class MinipayDemo extends React.Component {
     constructor(props) {
